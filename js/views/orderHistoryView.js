65d8c26794ae719bf9f1eed40e57d100d149738c
anhlehong <anh.anhle2004@gmail.com>
2024-08-21 00:03:33 +0700

    update price for order history

--- js/views/orderHistoryView.js
+++ js/views/orderHistoryView.js
@@ -1,135 +1,14 @@
-// class OrderHistoryView {
-//     constructor() {
-//         this.orderHistoryTable = document.getElementById('listOrderHistory');
-//     }
-
-//     renderOrderHistory(orders) {
-//         let orderHistoryHTML = '';
-
-//         orders.forEach((order, index) => {
-//             orderHistoryHTML += `
-//             <tr data-order-id="${index}">
-//                 <th scope="row">${index + 1}</th>
-//                 <td>${new Date(order.orderDate).toLocaleDateString()}</td>
-//                 <td>${order.totalAmount.toLocaleString()}đ</td>
-//                 <td>${this.getOrderStatus(order.isIssue)}</td>
-//             </tr>
-//         `;
-//         });
-
-//         this.orderHistoryTable.innerHTML = orderHistoryHTML;
-
-//         // Thêm sự kiện click vào mỗi dòng
-//         orders.forEach((order, index) => {
-//             const orderRow = document.querySelector(`tr[data-order-id="${index}"]`);
-//             orderRow.addEventListener('click', () => {
-//                 this.renderOrderDetail(order);
-//             });
-//         });
-//     }
-
-//     // renderOrderDetail(order) {
-//     //     const detailSection = document.getElementById('listDetailOrderHistory');
-
-//     //     detailSection.innerHTML = `
-//     //     <h3 style="padding-top: 10px; padding-bottom: 10px;">CHI TIẾT HÓA ĐƠN</h3>
-//     //     <hr style="margin: 0px; padding-bottom: 10px;">
-//     //     <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Họ tên:&nbsp;</span><span>${order.name}</span></div>
-//     //     <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Email:&nbsp;</span><span>${order.email}</span></div>
-//     //     <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Số điện thoại:&nbsp;</span><span>${order.phone}</span></div>
-//     //     <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Địa chỉ:&nbsp;</span><span>${order.address}</span></div>
-//     //     <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Hình Thức thanh toán:&nbsp;</span><span>${order.paymentMethod}</span></div>
-//     //     <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Ghi chú:&nbsp;</span><span>${order.note}</span></div>
-//     //     <table class="table">
-//     //         <thead>
-//     //             <tr>
-//     //                 <th scope="col">STT</th>
-//     //                 <th scope="col">Sản phẩm</th>
-//     //                 <th scope="col">Kích thước</th>
-//     //                 <th scope="col">Số lượng</th>
-//     //                 <th scope="col">Đơn giá</th>
-//     //                 <th scope="col">Thành tiền</th>
-//     //             </tr>
-//     //         </thead>
-//     //         <tbody>
-//     //             ${this.renderOrderItems(order.items)}
-//     //         </tbody>
-//     //     </table>
-//     //     <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Tổng tiền:&nbsp;</span><span><b>${order.totalAmount.toLocaleString()}đ</b></span></div>
-//     // `;
-//     // }
-//     renderOrderDetail(order) {
-//         const detailSection = document.getElementById('listDetailOrderHistory');
-
-//         detailSection.innerHTML = `
-//         <h3 style="padding-top: 10px; padding-bottom: 10px;">CHI TIẾT HÓA ĐƠN</h3>
-//         <hr style="margin: 0px; padding-bottom: 10px;">
-//         <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Họ tên:&nbsp;</span><span>${order.name}</span></div>
-//         <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Email:&nbsp;</span><span>${order.email}</span></div>
-//         <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Số điện thoại:&nbsp;</span><span>${order.phone}</span></div>
-//         <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Địa chỉ:&nbsp;</span><span>${order.address}</span></div>
-//         <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Hình Thức thanh toán:&nbsp;</span><span>${order.paymentMethod}</span></div>
-//         <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Ngày tạo hóa đơn:&nbsp;</span><span>${new Date(order.orderDate).toLocaleDateString()}</span></div>
-//         <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Ghi chú:&nbsp;</span><span>${order.note}</span></div>
-//         <table class="table">
-//             <thead>
-//                 <tr>
-//                     <th scope="col">STT</th>
-//                     <th scope="col">Sản phẩm</th>
-//                     <th scope="col">Kích thước</th>
-//                     <th scope="col">Số lượng</th>
-//                     <th scope="col">Đơn giá</th>
-//                     <th scope="col">Thành tiền</th>
-//                 </tr>
-//             </thead>
-//             <tbody>
-//                 ${this.renderOrderItems(order.items)}
-//             </tbody>
-//         </table>
-//         <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Tổng tiền:&nbsp;</span><span><b>${order.totalAmount.toLocaleString()}đ</b></span></div>
-//     `;
-//     }
-
-//     renderOrderItems(items) {
-//         let itemsHTML = '';
-//         let index = 1;
-
-//         Object.keys(items).forEach(key => {
-//             const item = items[key];
-//             itemsHTML += `
-//             <tr>
-//                 <td>${index++}</td>
-//                 <td>${item.item_name}</td>
-//                 <td>${item.size}</td>
-//                 <td>${item.quantity}</td>
-//                 <td>${item.unit_price.toLocaleString()}đ</td>
-//                 <td>${item.total_price.toLocaleString()}đ</td>
-//             </tr>
-//         `;
-//         });
-
-//         return itemsHTML;
-//     }
-
-
-//     getOrderStatus(isIssue) {
-//         // Kiểm tra trạng thái của đơn hàng dựa trên giá trị isIssue
-//         if (isIssue) {
-//             return 'Đã xuất kho'; // Trạng thái nếu isIssue là true
-//         } else {
-//             return 'Chưa xuất kho'; // Trạng thái nếu isIssue là false
-//         }
-//     }
-// }
-
-// export default OrderHistoryView;
-
 class OrderHistoryView {
     constructor() {
         this.orderHistoryTable = document.getElementById('listOrderHistory');
         this.paginationElement = document.getElementById('pagination');
         this.ordersPerPage = 5; // Số lượng đơn hàng hiển thị trên mỗi trang
         this.orders = []; // Thêm thuộc tính để lưu trữ danh sách orders
+    }
+
+    formatPrice(number) {
+        let formattedNumber = number.toLocaleString('vi-VN');
+        return formattedNumber + " VNĐ";
     }
 
     renderOrderHistory(orders, currentPage = 1) {
@@ -146,7 +25,7 @@ class OrderHistoryView {
                 <tr data-order-id="${index}">
                     <th scope="row">${startIndex + index + 1}</th>
                     <td>${new Date(order.orderDate).toLocaleDateString()}</td>
-                    <td>${order.totalAmount.toLocaleString()}đ</td>
+                    <td>${this.formatPrice(Number(order.totalAmount))}</td>
                     <td>${this.getOrderStatus(order.isIssue)}</td>
                 </tr>
             `;
@@ -232,7 +111,7 @@ class OrderHistoryView {
                     ${this.renderOrderItems(order.items)}
                 </tbody>
             </table>
-            <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Tổng tiền:&nbsp;</span><span><b>${order.totalAmount.toLocaleString()}đ</b></span></div>
+            <div style="padding-bottom: 15px;"><span style="font-weight: bold;">Tổng tiền:&nbsp;</span><span><b>${this.formatPrice(Number(order.totalAmount))}</b></span></div>
         `;
     }
 
@@ -242,14 +121,15 @@ class OrderHistoryView {
 
         Object.keys(items).forEach(key => {
             const item = items[key];
+            console.log("item.unit_price: ", typeof (item.unit_price))
             itemsHTML += `
                 <tr>
                     <td>${index++}</td>
                     <td>${item.item_name}</td>
                     <td>${item.size}</td>
                     <td>${item.quantity}</td>
-                    <td>${item.unit_price.toLocaleString()}đ</td>
-                    <td>${item.total_price.toLocaleString()}đ</td>
+                    <td>${this.formatPrice(Number(item.unit_price))}</td>
+                    <td>${this.formatPrice(Number(item.total_price))}</td>
                 </tr>
             `;
         });
